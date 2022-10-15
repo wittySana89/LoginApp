e0cf46f35daebfe51a240a07d03d245a988c187d
Sahan Wijayasekara <67058336+wittySana89@users.noreply.github.com>
2022-10-15 14:14:10 +1000

    Add files via upload

---
+++ Models/userControllerClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Signin.Models
+{
+    public class userControllerClass
+    {
+        public int Number { get; set; }
+        public string userName { get; set; }
+        public string Password { get; set; }
+    }
+}
+
